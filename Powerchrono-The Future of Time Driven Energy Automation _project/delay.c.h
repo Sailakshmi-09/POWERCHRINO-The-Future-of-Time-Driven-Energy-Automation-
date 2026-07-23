a8195ce13833dcc87c40f8861d8e6117d1759ab8
Sailakshmi-09 <kuppireddisai@gmail.com>
2026-07-23 15:58:15 +0530

    Add comments to delay function declarations
    Added comments to clarify function declarations for delay functions.

--- Powerchrono-The Future of Time Driven Energy Automation _project/delay.c.h
+++ Powerchrono-The Future of Time Driven Energy Automation _project/delay.c.h
@@ -1,5 +1,16 @@
-#include "types.h"
-void delay_us(u32);
-void delay_ms(u32);
-void delay_s(u32);
+#include "types.h"   // Includes user-defined data types such as u32
 
+// Function declaration to generate a delay in microseconds
+// Parameter:
+//   u32 - Delay duration in microseconds
+void delay_us(u32);
+
+// Function declaration to generate a delay in milliseconds
+// Parameter:
+//   u32 - Delay duration in milliseconds
+void delay_ms(u32);
+
+// Function declaration to generate a delay in seconds
+// Parameter:
+//   u32 - Delay duration in seconds
+void delay_s(u32);
