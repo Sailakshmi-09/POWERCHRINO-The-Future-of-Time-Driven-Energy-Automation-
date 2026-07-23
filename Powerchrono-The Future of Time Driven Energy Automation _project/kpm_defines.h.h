b599cce1daa5d723ed989191e0fb192a01f0b569
Sailakshmi-09 <kuppireddisai@gmail.com>
2026-07-23 16:09:10 +0530

    Refactor keypad pin definitions with comments

--- Powerchrono-The Future of Time Driven Energy Automation _project/kpm_defines.h.h
+++ Powerchrono-The Future of Time Driven Energy Automation _project/kpm_defines.h.h
@@ -1,8 +1,27 @@
-#define ROW0 16//@p1.16
-#define ROW1 17//@p1.17
-#define ROW2 18//@p1.18
-#define ROW3 19//@p1.19
-#define COL0 20//@p1.20
-#define COL1 21//@p1.21
-#define COL2 22
-#define COL3 23
+// ---------------- Keypad Row Pin Definitions ----------------
+
+// ROW0 is connected to Port 1, Pin 16 (P1.16)
+#define ROW0 16   // @P1.16
+
+// ROW1 is connected to Port 1, Pin 17 (P1.17)
+#define ROW1 17   // @P1.17
+
+// ROW2 is connected to Port 1, Pin 18 (P1.18)
+#define ROW2 18   // @P1.18
+
+// ROW3 is connected to Port 1, Pin 19 (P1.19)
+#define ROW3 19   // @P1.19
+
+// ---------------- Keypad Column Pin Definitions ----------------
+
+// COL0 is connected to Port 1, Pin 20 (P1.20)
+#define COL0 20   // @P1.20
+
+// COL1 is connected to Port 1, Pin 21 (P1.21)
+#define COL1 21   // @P1.21
+
+// COL2 is connected to Port 1, Pin 22 (P1.22)
+#define COL2 22   // @P1.22
+
+// COL3 is connected to Port 1, Pin 23 (P1.23)
+#define COL3 23   // @P1.23
