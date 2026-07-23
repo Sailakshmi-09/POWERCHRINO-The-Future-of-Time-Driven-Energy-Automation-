81064f7d86b735d543834c7741deca5cb9d73998
Sailakshmi-09 <kuppireddisai@gmail.com>
2026-07-23 16:25:48 +0530

    Update types.h.h

--- Powerchrono-The Future of Time Driven Energy Automation _project/types.h.h
+++ Powerchrono-The Future of Time Driven Energy Automation _project/types.h.h
@@ -1,10 +1,44 @@
+// ---------------- 32-bit Integer Data Types ----------------
+
+// Unsigned 32-bit integer
+// Range: 0 to 4,294,967,295
 typedef unsigned int u32;
+
+// Signed 32-bit integer
+// Range: -2,147,483,648 to 2,147,483,647
 typedef int s32;
+
+// ---------------- 16-bit Integer Data Types ----------------
+
+// Unsigned 16-bit integer
+// Range: 0 to 65,535
 typedef unsigned short u16;
+
+// Signed 16-bit integer
+// Range: -32,768 to 32,767
 typedef short int s16;
+
+// ---------------- 8-bit Integer Data Types ----------------
+
+// Unsigned 8-bit integer
+// Range: 0 to 255
 typedef unsigned char u8;
+
+// Signed 8-bit integer
+// Range: -128 to 127
 typedef char s8;
+
+// ---------------- Floating Point Data Types ----------------
+
+// 32-bit floating-point number (single precision)
 typedef float f32;
+
+// 64-bit floating-point number (double precision)
 typedef double f64;
+
+// ---------------- Constant Data Type ----------------
+
+// Unsigned constant 8-bit integer
+// The value cannot be modified after initialization
 typedef unsigned const char cu8;
 
