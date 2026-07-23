438703bed07841734d0ef10aca2b5dbe2c5dd772
Sailakshmi-09 <kuppireddisai@gmail.com>
2026-07-23 16:21:23 +0530

    Update pin_connect_block.h.h

--- Powerchrono-The Future of Time Driven Energy Automation _project/pin_connect_block.h.h
+++ Powerchrono-The Future of Time Driven Energy Automation _project/pin_connect_block.h.h
@@ -1,3 +1,9 @@
-#include "types.h"
-void CfgPortPinFunc(u32 ,u32 , u32);
+#include "types.h"   // Includes user-defined data types such as u32
+
+// Function declaration to configure the function of a specific port pin
+// Parameters:
+//   u32 - Port number (0 or 1)
+//   u32 - Pin number within the selected port
+//   u32 - Pin function value (GPIO, UART, SPI, I2C, EINT, etc.)
+void CfgPortPinFunc(u32, u32, u32);
 
